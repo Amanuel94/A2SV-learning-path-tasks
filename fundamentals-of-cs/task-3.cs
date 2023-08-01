c765cfad296fd22117489565c37524f9f77787a9
Amanuel94 <amanueltewodros94@gmail.com>
2023-08-01 19:32:56 +0300

    update PalindromChecker to ignore case and punctuation insensitive

--- fundamentals-of-cs/task-3.cs
+++ fundamentals-of-cs/task-3.cs
@@ -1,6 +1,20 @@
+using System.Text;
 namespace Tasks{
     public class PalindromeChecker{
+    
+
+        private string Transform(string word){
+            StringBuilder transformed_text = new StringBuilder();
+            foreach (var ch in word)
+            {
+                if(char.IsLetter(ch))
+                    transformed_text.Append(char.ToLower(ch));
+            }
+            return transformed_text.ToString();
+        }
         public bool CheckPalindrome(string word){
+            
+            word = Transform(word);
             int len_word = word.Length;
             for (int i = 0; i <= len_word/2; i++)
             {
