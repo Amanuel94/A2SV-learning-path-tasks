b2db4c97c9e90f373655c48d1900c105c4f32b7a
Amanuel94 <amanueltewodros94@gmail.com>
2023-08-02 14:59:47 +0300

    day 3 task 2

---
+++ object-oriented-programming/Task-2/Program.cs
@@ -0,0 +1,158 @@
+﻿// using Tasks;
+namespace Tasks
+{
+    static class Program
+    {
+
+
+        private static void CheckInvalidInput<T>(string input, decimal? upperBound = null)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                throw new Exception("Input cannot be empty.");
+            }
+
+            if (typeof(T) == typeof(decimal) || typeof(T) == typeof(Int32))
+            {
+                if (upperBound == null)
+                {
+                    throw new Exception("Upper bound cannot be null");
+                }
+                decimal output;
+                if (decimal.TryParse(input, out output))
+                {
+                    if (!(output >= 0 && output <= upperBound))
+                    {
+                        throw new Exception($"The Input cannot be greater than {upperBound} and lower than 0.");
+                    }
+
+                }
+                else
+                {
+                    throw new Exception("Input needs to be a number.");
+                }
+            }
+        }
+
+        // a method to continously prompt the user when there is an invalid input
+        private static T DisplayPrompt<T>(string prompt, decimal? upperBound = null)
+        {
+            Console.Write($"{prompt}: ");
+            string variable = Console.ReadLine().Trim();
+            try
+            {
+                CheckInvalidInput<T>(variable, upperBound);
+            }
+            catch (Exception error)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(error.Message);
+                Console.ResetColor();
+                return DisplayPrompt<T>(prompt, upperBound);
+            }
+
+            return (T)Convert.ChangeType(variable, typeof(T));
+        }
+
+
+
+        static void Main()
+        {
+
+            Library library = new Library("My Library", "Here");
+            while(true){
+                string prompt = "What do you want to do?"+
+                                "\n\t 1. Add Book" +
+                                "\n\t 2. Remove Book" +
+                                "\n\t 3. Add Media Item" +
+                                "\n\t 4. Add Media Item" +
+                                "\n\t 5. Print Catalog" +
+                                "\n\t 0. Exit";
+                int choice = DisplayPrompt<int>(prompt, 5);
+
+                switch (choice){
+
+                    case 1: 
+                            CreateBook(library);
+                            break;
+                    case 2: DeleteBook(library);
+                            break;
+                    case 3: CreateMedia(library);
+                            break;
+                    case 4: DeleteMedia(library);
+                            break;
+                    case 5: library.PrintCatalog();
+                            break;
+                    case 0: Environment.Exit(0); 
+                            break;
+                    // exit
+                }
+            }
+
+
+        }
+
+        private static void CreateBook(Library library){
+            string prompt;
+            prompt= "Enter the title of the book";
+            string title = DisplayPrompt<string>(prompt);
+            prompt= "Enter the Author of the book";
+            string author = DisplayPrompt<string>(prompt);
+            prompt= "Enter the ISBN of the book";
+            string ISBN = DisplayPrompt<string>(prompt);
+
+            prompt= "Enter the year the book is published";
+            int publicationYear = DisplayPrompt<int>(prompt, DateTime.Now.Year);
+
+            library.AddBook(new Book(title, author, ISBN, publicationYear));
+
+        }
+
+        private static void DeleteBook(Library library){
+            string prompt;
+            prompt= "Enter the ISBN of the book";
+            string ISBN = DisplayPrompt<string>(prompt);
+
+            foreach (Book book in library.Books)
+            {
+                if(book.ISBN.Equals(ISBN)){
+                    library.RemoveBook(book);
+                    return;
+                }
+            }
+
+            Console.WriteLine("ISBN not found in library.");
+        }
+
+        private static void CreateMedia(Library library){
+            string prompt;
+            prompt= "Enter the title of the media item";
+            string title = DisplayPrompt<string>(prompt);
+            prompt= "Enter the media type";
+            string type = DisplayPrompt<string>(prompt);
+
+            prompt= "Enter the duration of the media item in minutes";
+            // every media should be less than 100 days long
+            int duration = DisplayPrompt<int>(prompt, 60*24*100);
+
+            library.AddMediaItem(new MediaItem(title, type, duration));
+
+    }
+
+    private static void DeleteMedia(Library library){
+        string prompt;
+        prompt= "Enter the title of the media";
+        string title = DisplayPrompt<string>(prompt);
+
+        foreach (MediaItem media in library.MediaItems)
+        {
+            if(media.Title.Equals(title)){
+                library.RemoveMediaItem(media);
+            }
+        }
+
+        Console.WriteLine("Media Item not found in library.");
+        }
+
+}
+}
---
+++ object-oriented-programming/Task-2/task-2.cs
@@ -0,0 +1,80 @@
+namespace Tasks{
+public class Library{
+    private string Name{get; init;}
+    private string Address {get; init;}
+    public List<Book> Books{get; set;}
+    public List<MediaItem> MediaItems{get; set;}
+    public HashSet<string> ISBNPool;
+    public Library(string Name, string Address){
+        this.Name = Name;
+        this.Address = Address;
+        Books = new List<Book>();
+        MediaItems = new List<MediaItem>();
+        ISBNPool = new HashSet<string>();
+    }
+    public void AddBook(Book book){
+        Books.Add(book);
+        ISBNPool.Add(book.ISBN);
+    }
+    public void RemoveBook(Book book){
+        Books.Remove(book);
+        ISBNPool.Remove(book.ISBN);
+    }
+    public void AddMediaItem(MediaItem item){
+        MediaItems.Add(item);
+    }
+    public void RemoveMediaItem(MediaItem item){
+        MediaItems.Remove(item);
+    }
+    public void PrintCatalog(){
+        Console.WriteLine("Here is the list of all books in the library");
+        Console.WriteLine($"\tTitle\tAuthor\tISBN\tPublication Year");
+        foreach (Book item in Books)
+        {
+            Console.WriteLine($"\t{item.Title}\t{item.Author}\t{item.ISBN}\t{item.PublicationYear}");
+        }
+        Console.WriteLine("");
+        Console.WriteLine("Here is the list of all media items in the library");
+        Console.WriteLine($"\tTitle\tMedia Type\tDuration");
+        foreach (MediaItem item in MediaItems)
+        {
+            Console.WriteLine($"\t{item.Title}\t{item.MediaType}\t{item.Duration}");
+        }
+    }
+}
+
+public class Book{
+    public string Title{get; set;}
+    public string Author{get; set;}
+    public string ISBN{get; set;}
+    public int PublicationYear{get; set;}
+
+    
+
+    public Book(string Title, string Author, string ISBN, int PublicationYear){
+        this.Title = Title;
+        this.Author = Author;
+        this.ISBN = ISBN;
+        this.PublicationYear = PublicationYear;
+        
+        
+
+    }
+
+}
+
+public class MediaItem{
+    public string Title{get; set;}
+    public string MediaType{get; set;}
+    public int Duration{get; set;}
+
+    public MediaItem(string Title, string MediaType, int Duration){
+        this.Title = Title;
+        this.MediaType = MediaType;
+        this.Duration = Duration;
+    }
+
+
+
+}
+}
